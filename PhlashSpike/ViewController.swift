1f5ad67d0dcb4b961230f24854fcdbe6e51a4f69
Amy Nicholson <missamynicholson@gmail.com>
2016-06-12 12:17:35 +0100

    Initial Commit

---
+++ PhlashSpike/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PhlashSpike
+//
+//  Created by Amy Nicholson on 12/06/2016.
+//  Copyright © 2016 Amy Nicholson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
